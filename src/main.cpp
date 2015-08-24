4384488b0d56490906e37e55a3c1ae4fa4f4acf6
eshiho <shiho.elliptic@gmail.com>
2015-08-24 11:12:27 +0900

    [ADD] Time test code

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,38 @@
 int main(int, char**) {
   Fp::init("0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f");
   ECPoint::init();
-  ECPoint G = ECPoint(mpz_class(ES_ECC_X), mpz_class(ES_ECC_Y));
-  srand(time(0));
+  ECPoint G;
+  std::cout << "Start Fp p; p *= p(1000*1000 Times repeat)" << std::endl;
+  Fp p = mpz_class(time(0));
+  clock_t start = clock();
+  for (int i = 0; i < 1000; i++) {
+    for (int j = 0; j < 1000; j++) {
+      p *= p;
+    }
+  }
+  clock_t end = clock();
+  std::cout << "Finished Fp." << std::endl << "Time: " << ((double)end - start)/CLOCKS_PER_SEC << "sec" << std::endl;
+
+  std::cout << "Start G = 10000G(1000 Times repeat)" << std::endl;
+  G = ECPoint(mpz_class(ES_ECC_X), mpz_class(ES_ECC_Y));
+  start = clock();
+  for (int i = 0; i < 1000; i++) {
+    G = 10000*G;
+  }
+  end = clock();
+  std::cout << "Finished ECPoint." << std::endl << "Time: " << ((double)end - start)/CLOCKS_PER_SEC << "sec" << std::endl;
+
+  std::cout << "Start G = 2G(1000*1000 Times repeat)" << std::endl;
+  G = ECPoint(mpz_class(ES_ECC_X), mpz_class(ES_ECC_Y));
+  start = clock();
+  for (int i = 0; i < 1000; i++) {
+    for (int j = 0; j < 1000; j++) {
+      G = G+G;
+    }
+  }
+  end = clock();
+  std::cout << "Finished ECPoint." << std::endl << "Time: " << ((double)end - start)/CLOCKS_PER_SEC << "sec" << std::endl;
+/*
   mpz_class a = get_random_bytes(1024);
   std::cout << "a = " << a << std::endl;
 
@@ -33,4 +63,5 @@ int main(int, char**) {
     std::cout << "false";
   }
   std::cout << std::endl;
+  */
 }
